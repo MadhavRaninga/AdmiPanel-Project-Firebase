b81d746bab2fc5205fe76e6eb26a38c995ee01fc
Madhav Raninga <madhavraninga3@gmail.com>
2025-10-07 16:42:12 +0530

    m

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { collection, getDocs } from "firebase/firestore";
 import { db } from './Firebase/Firebase';
+import { Link } from 'react-router-dom';
 const Product = () => {
     let [data, setData] = useState([])
     useEffect(() => {
@@ -8,25 +9,31 @@ const Product = () => {
             const querySnapshot = await getDocs(collection(db, "users"));
             querySnapshot.forEach((doc) => {
                 console.log(doc.id, doc.data());
-                const products = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-                setData(products); // set state only once
+                let products = querySnapshot.docs.map(doc => doc.data())
+                setData(products);
             })
         });
         fetchdata()
     }, [])
     return (
-        <div className="product-container">
-            {data.map((e, i) => (
-                <div className="product-card" key={i}>
-                    <img src={e.url} alt={e.title} />
-                    <div className="product-info">
-                        <h1>{e.title}</h1>
-                        <p>{e.description}</p>
-                        <span>₹ {e.price}</span>
-                    </div>
-                </div>
-            ))}
-        </div>
+<div className="product-container">
+  {data.map((e, i) => (
+    <div className="product-card" key={i}>
+      <img src={e.url} alt={e.title} />
+      <div className="product-info">
+        <h1>{e.title}</h1>
+        <p>{e.description}</p>
+        <span>₹ {e.price}</span>
+      </div>
+    </div>
+  ))}
+<div className="back-button-container">
+  <Link to={'/'}>
+    <button className="back-button">Back To Home</button>
+  </Link>
+</div>
+</div>
+
     );
 }
 export default Product
